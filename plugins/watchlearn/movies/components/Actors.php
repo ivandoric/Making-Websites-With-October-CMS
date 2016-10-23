146095f305eaabe755b8dbe6628f035ed96e88b1
Ivan Doric <ivan.doric@gauss.hr>
2016-10-23 19:19:25 +0200

    Making websites with October CMS - Part 17 - Adding Options for Components

--- plugins/watchlearn/movies/components/Actors.php
+++ plugins/watchlearn/movies/components/Actors.php
@@ -13,12 +13,52 @@ class Actors extends ComponentBase
         ];
     }
 
+    public function defineProperties(){
+        return [
+            'results' => [
+                'title' => 'Number of Actors',
+                'description' => 'How  many actors do you want to display?',
+                'default' => 0,
+                'validationPattern' => '^[0-9]+$',
+                'validationMessage' => 'Only numbers allowed'
+            ],
+
+            'sortOrder' => [
+                'title' => 'Sort Actors',
+                'description' => 'Sort those actors',
+                'type' => 'dropdown',
+                'default' => 'name asc'
+            ]
+        ];
+    }
+
+    public function getSortOrderOptions(){
+        return [
+            'name asc' => 'Name (ascending)',
+            'name desc' => 'Name (descending)'
+        ];
+    }
+
     public function onRun(){
         $this->actors = $this->loadActors();
     }
 
     protected function loadActors(){
-        return Actor::all();
+        $query = Actor::all();
+
+        if($this->property('sortOrder') == 'name asc'){
+            $query = $query->sortBy('name');
+        }
+
+        if($this->property('sortOrder') == 'name desc'){
+            $query = $query->sortByDesc('name');
+        }
+
+        if($this->property('results') > 0){
+            $query = $query->take($this->property('results'));
+        }
+
+        return $query;
     }
 
     public $actors;
