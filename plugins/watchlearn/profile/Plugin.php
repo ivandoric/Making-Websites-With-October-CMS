42d478fadbd4cce3a39b643aea11b21e149d0e1b
Ivan Doric <ivan.doric@gauss.hr>
2016-11-26 19:32:08 +0100

    Making websites with October CMS - Part 24 - Extending User Plugin - Frontend

--- plugins/watchlearn/profile/Plugin.php
+++ plugins/watchlearn/profile/Plugin.php
@@ -2,6 +2,7 @@
 
 use System\Classes\PluginBase;
 use Rainlab\User\Controllers\Users as UsersController;
+use Rainlab\User\Models\User as UserModel;
 
 class Plugin extends PluginBase
 {
@@ -14,6 +15,13 @@ class Plugin extends PluginBase
     }
 
     public function boot(){
+
+        UserModel::extend(function ($model){
+            $model->addFillable([
+                'facebook',
+                'bio'
+            ]);
+        });
 
         UsersController::extendFormFields(function($form, $model, $context){
             $form->addTabFields([
