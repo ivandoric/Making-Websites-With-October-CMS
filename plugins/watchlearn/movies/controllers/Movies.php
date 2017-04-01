32bfcb648bb5727487f74ac64542bbdbb4ddf445
Ivan Dorić <ivan.doric@burza.hr>
2017-04-01 10:42:50 +0200

    Making Websites With October CMS - Part 36 - Relation Manager

--- plugins/watchlearn/movies/controllers/Movies.php
+++ plugins/watchlearn/movies/controllers/Movies.php
@@ -5,11 +5,12 @@ use BackendMenu;
 
 class Movies extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\ReorderController'];
+    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\ReorderController', 'Backend.Behaviors.RelationController'];
     
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
+    public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
     {
